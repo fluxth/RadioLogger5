723d35c7a075ad4ffffc7760b07a0a7dbbe38a4f
fluxth <fuu.thitat@gmail.com>
2020-01-26 02:30:46 +0700

    Added wsgi for web GUI

--- gui.py
+++ gui.py
@@ -23,15 +23,6 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 db = SQLAlchemy(app, model_class=Base)
 
-
-# !! TEMP FOR DEV
-@app.after_request
-def apply_caching(response):
-    response.headers['Access-Control-Allow-Origin'] = "http://127.0.0.1:3000"
-    response.headers['Access-Control-Allow-Headers'] = 'Authorization, X-Requested-With'
-    return response
-
-
 # Route to /api/
 app.route('/api/<path:path>')           (server.serve_api)
 
--- gui_dev.py
+++ gui_dev.py
@@ -1,3 +1,10 @@
 from gui import app
 
+# !! TEMP FOR DEV
+@app.after_request
+def apply_caching(response):
+    response.headers['Access-Control-Allow-Origin'] = "http://127.0.0.1:3000"
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization, X-Requested-With'
+    return response
+
 app.run(debug=True)
