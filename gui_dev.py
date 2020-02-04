54ff74dd4b971da1fb812ae3503b57b83a441bf9
fluxth <fuu.thitat@gmail.com>
2020-02-05 01:19:38 +0700

    Updated GUI dev environment

--- gui_dev.py
+++ gui_dev.py
@@ -1,10 +1,9 @@
 from gui import app
 
-# !! TEMP FOR DEV
 @app.after_request
 def apply_caching(response):
-    response.headers['Access-Control-Allow-Origin'] = "http://127.0.0.1:3000"
+    response.headers['Access-Control-Allow-Origin'] = '*' 
     response.headers['Access-Control-Allow-Headers'] = 'Authorization, X-Requested-With'
     return response
 
-app.run(debug=True)
+app.run(host='0.0.0.0', port=5003, debug=True)
