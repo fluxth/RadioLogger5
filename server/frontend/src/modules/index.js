ef26e53c870f1be967a0344e69d2bf1bb5f46a2a
fluxth <fuu.thitat@gmail.com>
2020-02-06 02:26:08 +0700

    Added /v1 to API endpoints

--- server/frontend/src/modules/index.js
+++ server/frontend/src/modules/index.js
@@ -2,8 +2,8 @@ import auth from './auth'
 import stations from './stations'
 import history from './station/history'
 
-export const API_URL = 'http://127.0.0.1:5003/api'
-// export const API_URL = '/api'
+export const API_URL = 'http://127.0.0.1:5003/api/v1'
+// export const API_URL = '/api/v1'
 export const AUTH_STORAGE_KEY = 'authCredentials'
 
 export default {
