ecf75196ee61402388ccdfef382b201f90d3399f
fluxth <fuu.thitat@gmail.com>
2020-03-17 14:41:50 +0700

    Cool93 station hotfix

--- stations/Cool93.py
+++ stations/Cool93.py
@@ -4,20 +4,16 @@ class Cool93Station(Station):
     _NAME = 'Cool93'
     _SHORTNAME = 'C93'
 
-    _URL = 'http://www.coolism.net/mobile-xml/fahrenheit/rcs/BILLBOARD.ASC'
+    _URL = 'http://www.coolism.net/radio/dab/nowplaying.php'
 
     def parseResponse(self, payload):
-        track = payload.content.decode('tis-620').split('\r\n')[0]
 
-        try:
-            title = track[:50].strip()
-        except IndexError:
-            title = ''
+        data = payload.json()
 
-        try:
-            artist = track[50:80].strip().replace('_', ' ')
-        except IndexError:
-            artist = ''
+        entity = data['data']['entities']
+
+        title = entity['song']
+        artist = entity['artist']
 
         return Metadata(title=title, artist=artist)
 
